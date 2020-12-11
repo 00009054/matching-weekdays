2a39be6c4edbe1a92a0707ec2e151c5823f6218a
00009054 <00009054.wiut@gmail.com>
2020-12-11 09:43:35 +0500

    create form that matches days of the week

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var elMatchForm = document.querySelector('.js-form');
+var elNumberInput = elMatchForm.querySelector('.number-input');
+
+var elResult = document.querySelector('.result');
+var message = '';
+
+elMatchForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  var number = Number(elNumberInput.value);
+ 
+  if (number === 1){
+    message += 'Monday';
+  }
+  if (number === 2){
+    message += 'Tuesday';
+  } 
+  if (number === 3){
+    message += 'Wednesday';
+  } 
+  if (number === 4){
+    message += 'Thursday';
+  } 
+  if (number === 5){
+    message += 'Friday';
+  } 
+  if (number === 6){
+    message += 'Saturday';
+  } 
+  if (number === 7){
+    message += 'Sunday';
+  } 
+  if (number <= 0 || number > 7){
+    message += '404 Error. Not Found';
+    elResult.classList.add('alert-danger')
+  }
+
+  elResult.classList.remove('d-none');
+  elResult.textContent = message;
+});
